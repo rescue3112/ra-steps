e5e5b923e9b0acf89379b5b4fc8955eda3d62ab6
AppVeyor <ci@appveyor.com>
2023-08-02 14:45:34 +0700

    1commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Steps from "./components/Steps"
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Steps />
+    </>
+  );
+}
+
+export default App;
